30c73ab0856c54466f19d2f2ccaade69174219c1
Nishant-jarang <nishant.jarang@mealawe.com>
2025-09-19 17:24:34 +0530

    Added the vendor ledger and wallet in vendorFirm...

--- src/app/vendor-firm-view/vendor-firm-view.module.ts
+++ src/app/vendor-firm-view/vendor-firm-view.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { VendorFirmViewRoutingModule } from './vendor-firm-view-routing.module';
 import { VendorFirmViewComponent } from './vendor-firm-view.component';
 import { VendorDetailsComponent } from '../common-components/vendor-details/vendor-details.component';
+import { WalletDetailsComponent } from './wallet-details/wallet-details.component';
+import { LedgerDetailsComponent } from './ledger-details/ledger-details.component';
 
 
 @NgModule({
   declarations: [
     VendorFirmViewComponent,
-    VendorDetailsComponent
+    VendorDetailsComponent,
+    WalletDetailsComponent,
+    LedgerDetailsComponent
   ],
   imports: [
     CommonModule,
