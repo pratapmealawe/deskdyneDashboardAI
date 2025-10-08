c41a8fe3a901aea62156ae23894059f0cdbf948b
Apurv-Patole <apurv.patole@mealawe.com>
2025-10-08 11:24:11 +0530

    Outlet change

--- src/app/outlet/add-outlet/add-outlet.module.ts
+++ src/app/outlet/add-outlet/add-outlet.module.ts
@@ -3,7 +3,17 @@ import { CommonModule } from '@angular/common';
 import { AddOutletComponent } from './add-outlet.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddOutletRoutingModule } from './add-outlet-routing.module';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -13,7 +23,18 @@ import { AddOutletRoutingModule } from './add-outlet-routing.module';
     CommonModule,
     FormsModule,
     AddOutletRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatDividerModule,
   ]
 })
 export class AddOutletModule { }
