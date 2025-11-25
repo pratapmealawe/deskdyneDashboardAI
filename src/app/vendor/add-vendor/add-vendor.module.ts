ca25419405b1ee6189205a2d5f8903eff9038e55
Saakshi Das <saakshi.das@mealawe.com>
2025-11-25 02:01:05 -0800

    changes in modules  outlet - org details - searchorg - orgview

--- src/app/vendor/add-vendor/add-vendor.module.ts
+++ src/app/vendor/add-vendor/add-vendor.module.ts
@@ -6,7 +6,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { VendorComplianceComponent} from './vendor-compliance/vendor-compliance.component';
 import { PdfuploadComponent } from 'src/app/pdfupload/pdfupload.component';
-import {MatRadioModule} from '@angular/material/radio';
 import { SetGeolocationModule } from 'src/app/set-geolocation/set-geolocation.module';
 import { MaterialModule } from 'src/app/material.module';
 
