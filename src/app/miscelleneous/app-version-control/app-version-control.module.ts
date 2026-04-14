963fa0f9d2bba487db2da22cbbdc7e2c22a99cb5
Omkar Dhapte <omkar.dhapte@mealawe.com>
2026-04-14 15:07:54 +0530

    build iisue solve

--- src/app/miscelleneous/app-version-control/app-version-control.module.ts
+++ src/app/miscelleneous/app-version-control/app-version-control.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTooltipModule } from '@angular/material/tooltip';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppVersionControlRoutingModule } from './app-version-control-routing.module';
 import { AppVersionControlComponent } from './app-version-control.component';
+import { AddEditAppVersionControlComponent } from './add-edit-app-version-control/add-edit-app-version-control.component';
 
 @NgModule({
   declarations: [
-    AppVersionControlComponent
+    AppVersionControlComponent,
+    AddEditAppVersionControlComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +25,9 @@ import { AppVersionControlComponent } from './app-version-control.component';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatDialogModule
   ]
 })
 export class AppVersionControlModule { }
